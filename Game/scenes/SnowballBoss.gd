3485644b1cb49c3ab974a8d4329df8d92e1eff7a
pintti <allu.tuovinen@gmail.com>
2023-01-27 19:12:34 +0200

    VITUN VITTU SAATANA

--- Game/scenes/SnowballBoss.gd
+++ Game/scenes/SnowballBoss.gd
@@ -5,21 +5,37 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var pos = Vector2.ZERO
-var screenSize = get_viewport().get_visible_rect().size
+var screenSize = Vector2.ZERO
+var middle = Vector2.ZERO
+var moveTween = Tween.new()
+var moving = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#hide()
+	screenSize = get_viewport().get_visible_rect().size
+	middle = screenSize / 2
 	self.position = get_start_pos()
 	
+	add_child(moveTween)
+	moveTween.interpolate_property(self, "position:y", self.position.y, middle.y-middle.y/2, 3)
+	moveTween.start()
 
-	
+
 func get_start_pos():
-	var middle = screenSize / 2
-	middle[1] = -50
-	return middle
-	
+	var start_pos = middle
+	start_pos[1] = -50
+	return start_pos
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	$Sprite.rotation += 1 * delta
+	
+	if self.position.y == middle.y-middle.y/2 and moving:
+		moving = false
+		moveTween.interpolate_property(self, "position:x", self.position.x, middle.x-middle.x/2, 3)
+	
+	if self.position.x == middle.x-middle.x/2:
+		moveTween.interpolate_property(self, "position:x", self.position.x, middle.x+middle.x/2, 5)
+	elif self.position.x == middle.x+middle.x/2:
+		moveTween.interpolate_property(self, "position:x", self.position.x, middle.x-middle.x/2, 5)
